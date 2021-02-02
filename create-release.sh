27600559bf4eab59239e4c98dd3be00d47f68c39
Michael Mercier <michael.mercier@ryax.tech>
2021-02-02 14:45:27 +0100

    Fix create relase script

--- create-release.sh
+++ create-release.sh
@@ -7,7 +7,7 @@ VERSION=${1:-"latest"}
 FROM_VERSION=${2:-"staging"}
 
 # ryaxpkgs is here for api proxy image (TODO move it to ./api)
-SERVICES="./webui ./repository ./api ./core ./adm ./cli ./studio ./effects/builder ./effects/orchestrator ./ryaxpkgs"
+SERVICES="./webui ./repository ./api ./core ./adm ./cli ./studio ./effects/orchestrator ./ryaxpkgs"
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -15,11 +15,12 @@ for service in $SERVICES
 do
     echo === Entering $service
     cd $service
-    git checkout "$FROM_VERSION"
+    git checkout master
     git pull
+    git checkout "$FROM_VERSION"
     if [[ $(git tag --points-at HEAD | grep $VERSION) == $VERSION ]]
     then
-        echo == Version $VERSION already on the head of master for $service
+        echo == Version $VERSION already on the reference $FROM_VERSION for $service
     else
         echo == Updating the $VERSION to the head of master
         git tag --delete "$VERSION" || true
