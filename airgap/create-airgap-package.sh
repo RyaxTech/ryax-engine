cbd3761fc86a00dfefd609e81fdb988176ad4000
Michael Mercier <michael.mercier@ryax.tech>
2025-12-26 16:36:15 +0100

    Fix helm charts

--- airgap/create-airgap-package.sh
+++ airgap/create-airgap-package.sh
@@ -2,8 +2,8 @@
 set -e
 # set -x
 
-RYAX_CONFIG=${1:-./ryax-airgap-helm-values.yaml}
 SELF_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+RYAX_CONFIG=${1:-$SELF_DIR/ryax-airgap-helm-values.yaml}
 
 $SELF_DIR/generate-image-list.sh $1 > /tmp/image-list.txt
 $SELF_DIR/create-k3s-tarball.sh /tmp/image-list.txt
--- airgap/generate-image-list.sh
+++ airgap/generate-image-list.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SELF_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-helm template ../chart --values $SELF_DIR/ryax-airgap-helm-values.yaml \
+helm template $SELF_DIR/../chart --values $SELF_DIR/ryax-airgap-helm-values.yaml | tee /tmp/tmpl.yaml \
   >  >(grep 'image: ' | sed 's/.*image: //' | sed 's/\"\(.*\)\"/\1/' | sort | uniq)
   2> >(grep 'repo')
