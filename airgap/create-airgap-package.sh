ecbf25ba37a7f1e4464e4710c6a7c2db9f06d0ed
Michael Mercier <michael.mercier@ryax.tech>
2026-01-16 11:24:53 +0100

    Fix airgap script

--- airgap/create-airgap-package.sh
+++ airgap/create-airgap-package.sh
@@ -5,7 +5,7 @@ set -e
 SELF_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 RYAX_CONFIG=${1:-$SELF_DIR/ryax-airgap-helm-values.yaml}
 
-$SELF_DIR/generate-image-list.sh $1 > /tmp/image-list.txt
+$SELF_DIR/generate-image-list.sh $RYAX_CONFIG > /tmp/image-list.txt
 $SELF_DIR/create-k3s-tarball.sh /tmp/image-list.txt
 $SELF_DIR/create-helm-package.sh
 
